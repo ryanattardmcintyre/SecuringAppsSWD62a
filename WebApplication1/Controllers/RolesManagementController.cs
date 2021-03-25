db782d477b7de390a9a588072407800e655c91d3
ryan.attard <ryan.attard@mcast.edu.mt>
2021-03-25 13:14:02 +0100

    RolesAllocation

--- WebApplication1/Controllers/RolesManagementController.cs
+++ WebApplication1/Controllers/RolesManagementController.cs
@@ -3,16 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     [Authorize(Roles ="ADMIN,TEACHER")]
     public class RolesManagementController : Controller
     {
-        public IActionResult Create()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesManagementController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
+
+        public IActionResult Index()
+        {
+            RolesManagementModel model = new RolesManagementModel();
+            model.Roles = _roleManager.Roles.ToList();
+            model.Users = _userManager.Users.ToList();
+            
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> AllocateRole(string role, string user, string btnName)
+        { var returnedUser = await _userManager.FindByNameAsync(user);
+            if(btnName=="Allocate")
+            {
+                if (returnedUser != null)
+                {
+                    await _userManager.AddToRoleAsync(returnedUser, role);
+
+                    TempData["message"] = "successfully allocated";
+                }
+                else
+                {
+                    TempData["error"] = "user not found";
+                }
+            }
+            else
+            {
+                //deallocate
+                if (returnedUser != null)
+                {
+                    await _userManager.RemoveFromRoleAsync(returnedUser, role);
+
+                    TempData["message"] = "successfully deallocated";
+                }
+                else
+                {
+                    TempData["error"] = "user not found";
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+       
     }
 }
---
+++ WebApplication1/Models/RolesManagementModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class RolesManagementModel
+    {
+        public List<IdentityRole> Roles { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+    }
+}
